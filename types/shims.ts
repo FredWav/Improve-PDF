687e80132211df43442c3d2e7e7975902b011b13
FredWav <99555684+FredWav@users.noreply.github.com>
2025-08-31 00:51:06 +0200

    Condense module declarations in shims.ts

--- types/shims.ts
+++ types/shims.ts
@@ -1,10 +1,2 @@
-// types/shims.ts
-declare module 'epub-gen' {
-  const EPub: any;
-  export default EPub;
-}
-
-declare module 'ejs' {
-  const ejs: any;
-  export default ejs;
-}
+declare module 'epub-gen' { const EPub: any; export default EPub }
+declare module 'ejs' { const ejs: any; export default ejs }
