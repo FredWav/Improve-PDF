51fce0d6a35b1065052c0b2f9f4f9438574ca859
FredWav <99555684+FredWav@users.noreply.github.com>
2025-08-30 16:32:48 +0200

    Fix formatting in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './lib/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
@@ -13,12 +13,12 @@ module.exports = {
           'ui-sans-serif','system-ui','Segoe UI','Roboto','Inter',
           'Helvetica Neue','Arial','Noto Sans','Apple Color Emoji',
           'Segoe UI Emoji','Segoe UI Symbol'
-        ],
+        ]
       },
       boxShadow: {
-        soft: '0 1px 2px rgba(0,0,0,0.05), 0 10px 20px rgba(0,0,0,0.03)',
-      },
-    },
+        soft: '0 1px 2px rgba(0,0,0,0.05), 0 10px 20px rgba(0,0,0,0.03)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
