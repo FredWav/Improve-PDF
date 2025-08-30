62d96d318904618058ebd07ebe9b6104986569b8
FredWav <99555684+FredWav@users.noreply.github.com>
2025-08-30 16:28:55 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './lib/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}', // au cas où
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['ui-sans-serif', 'system-ui', 'Segoe UI', 'Roboto', 'Inter', 'Helvetica Neue', 'Arial', 'Noto Sans', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
+        sans: [
+          'ui-sans-serif','system-ui','Segoe UI','Roboto','Inter',
+          'Helvetica Neue','Arial','Noto Sans','Apple Color Emoji',
+          'Segoe UI Emoji','Segoe UI Symbol'
+        ],
       },
       boxShadow: {
         soft: '0 1px 2px rgba(0,0,0,0.05), 0 10px 20px rgba(0,0,0,0.03)',
