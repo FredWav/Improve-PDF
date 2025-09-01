5ef725abbd512c5e8e2618122540d910eccca2ec
FredWav <99555684+FredWav@users.noreply.github.com>
2025-09-01 13:17:17 +0200

    Create Inngest client for multi-file usage
    Creates and exports the Inngest client for use in multiple files.

---
+++ inngest/client.ts
@@ -0,0 +1,4 @@
+import { Inngest } from 'inngest';
+
+// Crée et exporte le client Inngest pour qu'on puisse l'utiliser dans plusieurs fichiers
+export const inngest = new Inngest({ id: 'Improve-PDF' });
