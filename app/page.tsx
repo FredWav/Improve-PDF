66d0f82caa5afbcfcca68a7e312fc3aab662a8c1
FredWav <99555684+FredWav@users.noreply.github.com>
2025-09-01 14:52:15 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import HomePageClient from '@/components/HomePageClient'
+import { HomePageClient } from '@/components/HomePageClient';
 
 export default function Home() {
-  return <HomePageClient />
+  return <HomePageClient />;
 }
